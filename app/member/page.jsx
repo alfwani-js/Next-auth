2e1324d1eeb06f7cb38d69acd74295c926d93687
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-10-21 22:10:43 +0300

    protect member page

--- app/member/page.jsx
+++ app/member/page.jsx
@@ -1,9 +1,20 @@
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { options } from "@/app/api/auth/[...nextauth]/options"
 
+const Member= async() => {
+  const session = await getServerSession(options)
 
-const Member=() => {
+  if(!session){
+    redirect("/api/auth/signin?callbackUrl=/member")
+  }
   return (
     <div>
     <h1> Member Server Session</h1>
+    <p>
+      {session?.user?.email}
+    </p>
+    <p>{session?.user?.email}</p>
       
     </div>
   )
