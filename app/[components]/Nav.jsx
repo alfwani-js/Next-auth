99daf72d59ab6ce37d28619c512ef0c86cdbeb74
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-10-21 19:39:09 +0300

    update logins

--- app/[components]/Nav.jsx
+++ app/[components]/Nav.jsx
@@ -1,6 +1,10 @@
 import Link from "next/link"
+import { getServerSession} from "next-auth"
+import { options } from "../api/auth/[...nextauth]/options"
 
-const Nav = () => {
+
+const Nav = async() => {
+  const session = await getServerSession(options)
   return (
     <div>
        <header className="bg-gray-600 text-gray-100">
@@ -12,6 +16,12 @@ const Nav = () => {
                 <Link href="/ClientMember">Client Member</Link>
                 <Link href="/member">member</Link>
                 <Link href="/public">Public</Link>
+                {session ? (<Link href="/api/auth/signout?callbackurl=/">Logout</Link>
+                ):(
+                    <Link href="/api/auth/signin">Login</Link>
+                )
+                }
+
             </div>
 
         </nav>
