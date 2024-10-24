e1aee68b4d8969b971388a60448670a75a59d982
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-10-24 14:02:45 +0300

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,20 +1,23 @@
-import {withAuth} from "next-auth/middleware";
-import  {NextResponse} from "next/server";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
-    function middleware(req){
-        console.log("url:"+req.nextUrl.pathname);
-        console.log("role:"+req.nextauth.role);
+  function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token.role);
 
-        if(req.nextUrl.pathname.startsWith("/createuser") &&
-         req.nextauth.role !== "admin"){
-            return NextResponse.rewrite(new URL("/denied", req.url));
+    if (
+      req.nextUrl.pathname.startsWith("/createuser") &&
+      req.nextauth.token.role != "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", req.url));
     }
-},
-{callbacks:{
-    authorized:({token}) =>!!token,
-},
-}
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
 );
 
-export const config = {matcher:["/createuser"]};
+export const config = { matcher: ["/createuser"] };
