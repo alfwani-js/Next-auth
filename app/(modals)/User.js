93174224c8f71d1f656550dd8cfc2873aa523875
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-10-24 15:01:53 +0300

    schema setup

---
+++ app/(modals)/User.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.Promise = global.Promise;
+
+const UserSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+
+},{
+    timestamps: true
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default User;
